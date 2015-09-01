b8379a544156ed92d6c0c50c447adbae6639bf51
sipho ngubonde <mail.sipho@gmail.com>
2015-09-01 12:05:38 +0200

    solution added

---
+++ TransactionScript/TransactionScript/Application/BidOnAuctionService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using TransactionScript.Domain;
+
+namespace TransactionScript.Application
+{
+    public class BidOnAuctionService
+    {
+        private BidOnAuctionCommand _bidOnAuctionCommand;
+
+        public BidOnAuctionService(BidOnAuctionCommand bidOnAuctionCommand)
+        {
+            _bidOnAuctionCommand = bidOnAuctionCommand;
+        }
+
+        public void Bid(Guid auctionId, Guid memberId, decimal amount, DateTime dateOfBid)
+        {
+            _bidOnAuctionCommand.Execute();                             
+         }                      
+    }
+}
---
+++ TransactionScript/TransactionScript/Domain/Auction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TransactionScript.Domain
+{
+    public class Auction 
+    {
+        public Guid Id { get; set; }
+        public Guid ListingId { get; set; }
+        public DateTime EndsAt { get; set; }
+        public decimal StartingPrice { get; set; }
+        public decimal WinningBid { get; set; }
+        public decimal WinninBidderMaximumBid { get; set; }
+        public Guid WinningBidder { get; set; }                    
+    }
+}
